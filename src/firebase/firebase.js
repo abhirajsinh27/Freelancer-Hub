3210ff1c8c4202abccefaedf636d083f1cf7f9b4
abhirajsinh27 <abhirajsinhvala76@gmail.com>
2026-03-20 21:35:22 +0530

    Secured Firebase config using env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC5ihdgqNJjdsbL7tQoMPLVGbS2uS-bkwE",
-  authDomain: "freelancer-marketplace-d9981.firebaseapp.com",
-  projectId: "freelancer-marketplace-d9981",
-  storageBucket: "freelancer-marketplace-d9981.firebasestorage.app",
-  messagingSenderId: "83896917768",
-  appId: "1:83896917768:web:29ab710be5c339057296bd",
-  measurementId: "G-87VZ7B7J1D"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 
